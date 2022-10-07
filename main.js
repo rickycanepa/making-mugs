8e2fa5fb493544c019560f23ecac68b1c4ec4e88
Ricky Canepa <rickycanepa.rc@gmail.com>
2022-10-07 10:46:59 -0500

    making mugs

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (let i = 0; i < clay.length; i++) {
+   const mug = 'coffee mug';
+   toFireInKiln.push(clay[i] + ' ' + mug);
+}
+
+console.log(toFireInKiln)
